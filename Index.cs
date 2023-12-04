851cf83bc971df9c167f8213a5c5dd2be2cef19a
MRZ0123 <manuelmarz2004@gmail.com>
2023-12-04 15:23:41 +0100

    added add metadata field function

--- Index.cs
+++ Index.cs
@@ -4,6 +4,18 @@ namespace Manuel
 {
     class Index
     {
+
+        /** Added by: Manuel
+         * 
+         * function used to get the position of a metadafield by the id of a shopping list
+         * 
+         */
+        public void AddMetadataField(ShoppingList.Metadata metadata)
+        {
+
+        }
+
+
         /** Added by: Manuel
          * 
          * function used to get the position of a metadafield by the id of a shopping list
