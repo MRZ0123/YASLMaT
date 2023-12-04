f28685e138f6e5f703aa03a873026f43742afb71
MRZ0123 <manuelmarz2004@gmail.com>
2023-12-04 15:20:14 +0100

    added find list by id function

--- Index.cs
+++ Index.cs
@@ -4,6 +4,14 @@ namespace Manuel
 {
     class Index
     {
-
+        /** Added by: Manuel
+         * 
+         * function used to get the position of a metadafield by the id of a shopping list
+         * 
+         */
+        public int findListById(Config.Content currentConfig, string id)
+        {
+            return 0;
+        }
     }
 }
