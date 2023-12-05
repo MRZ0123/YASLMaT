2c4ce51a10c3c2deaaaa31e75a427604ba109c33
MRZ0123 <manuelmarz2004@gmail.com>
2023-12-05 12:02:22 +0100

    more tests

--- Program.cs
+++ Program.cs
@@ -19,26 +19,47 @@ namespace YASLMAT
             string configFileLocation = "./conf.ig";
 
             // ***** DEBUG MSG *****
-            if (DEBUG) {Console.WriteLine($"Checking for config file at \"{configFileLocation}\"."); }
+            if (DEBUG) { Console.WriteLine($"Checking for config file at \"{configFileLocation}\"."); }
             // **   -**#####**-   **
 
             if (!Config.Check(configFileLocation))
             {
-                
+
                 // ***** DEBUG MSG *****
                 if (DEBUG) { Console.WriteLine($"No config present.\nCreating config at \"{configFileLocation}\"."); }
                 // **   -**#####**-   **
 
                 Config.Create(configFileLocation);
             }
+            else
+            {
+                // ***** DEBUG MSG *****
+                if (DEBUG) { Console.WriteLine($"A config has been found."); }
+                // **   -**#####**-   **
+            }
+            
+            // ***** DEBUG MSG *****
+            if (DEBUG) {Console.WriteLine($"Reading from config."); }
+            // **   -**#####**-   **
+            
             Config.Content currentConfig = Config.Read(configFileLocation);
 
             // ***** DEBUG MSG *****
             if (DEBUG) { Console.WriteLine($"Current config content is:\n{currentConfig}"); }
             // **   -**#####**-   **
 
+            
+            // ***** DEBUG MSG *****
+            if (DEBUG) {Console.WriteLine($"Checking if there's an Shlindex file."); }
+            // **   -**#####**-   **
+            
             if (!Shlindex.Check(currentConfig))
             {
+                
+                // ***** DEBUG MSG *****
+                if (DEBUG) {Console.WriteLine($"No Shlindexfile found at\"{currentConfig.IndexFileLocation}\""); }
+                // **   -**#####**-   **
+                
                 Console.WriteLine(currentConfig.Language == "DE" ? "" : "");
             }
             ShoppingList.Remove(currentConfig, "12345");
