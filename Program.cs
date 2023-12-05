d2a0130fae846cb3504fc11f95043adf87d9706d
MRZ0123 <manuelmarz2004@gmail.com>
2023-12-05 11:32:07 +0100

    debug strings

--- Program.cs
+++ Program.cs
@@ -15,17 +15,33 @@ namespace YASLMAT
          */
         static void Main(string[] args)
         {
+            bool DEBUG = true;
             string configFileLocation = "./conf.ig";
-            if(! Config.Check(configFileLocation) )
+
+            // ***** DEBUG MSG *****
+            if (DEBUG) {Console.WriteLine($"Checking for config file at \"{configFileLocation}\"."); }
+            // **   -**#####**-   **
+
+            if (!Config.Check(configFileLocation))
             {
+                
+                // ***** DEBUG MSG *****
+                if (DEBUG) { Console.WriteLine($"No config present.\nCreating config at \"{configFileLocation}\"."); }
+                // **   -**#####**-   **
+
                 Config.Create(configFileLocation);
             }
             Config.Content currentConfig = Config.Read(configFileLocation);
 
-            Console.WriteLine(currentConfig.ToString());
+            // ***** DEBUG MSG *****
+            if (DEBUG) { Console.WriteLine($"Current config content is:\n{currentConfig}"); }
+            // **   -**#####**-   **
+
+            if (!Shlindex.Check(currentConfig))
+            {
+                Console.WriteLine(currentConfig.Language == "DE" ? "" : "");
+            }
             ShoppingList.Remove(currentConfig, "12345");
-            if(! Manuel.Index.Check(currentConfig))
-            Console.WriteLine(currentConfig.Language=="DE" ? "":"");
         }
 
     }
