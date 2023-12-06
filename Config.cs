c919d51353b22a0e775ba517cb8aba0b922fe395
MRZ0123 <manuelmarz2004@gmail.com>
2023-12-06 11:40:36 +0100

    turned language exception into writeline with exit

--- Config.cs
+++ Config.cs
@@ -32,7 +32,9 @@ namespace Manuel
                             _Language = value.ToUpper();
                             break;
                         default:
-                            throw new ArgumentException($"\n\n---------- German ----------\n\"{value}\" ist keine der unterstützten Sprachen: DE | EN\nBitte tragen Sie die gewollte Sprache, wie in dem internationalem Standard ISO 639-1 beschrieben, ein.\n\n---------- English ----------\n\"{value}\" is not one of the valid languages: DE | EN\nPlease insert the Language you want as described in the ISO 639-1 international standard.\n");
+                            Console.WriteLine($"\n\n---------- German ----------\n\"{value}\" ist keine der unterstützten Sprachen: DE | EN\nBitte tragen Sie die gewollte Sprache, wie in dem internationalem Standard ISO 639-1 beschrieben, ein.\n\n---------- English ----------\n\"{value}\" is not one of the valid languages: DE | EN\nPlease insert the Language you want as described in the ISO 639-1 international standard.");
+                            Environment.Exit(1);
+                            break;
                     }
                 }
             }
@@ -50,7 +52,9 @@ namespace Manuel
                         this.Language = language.ToUpper();
                         break;
                     default:
-                        throw new ArgumentException($"\n\n---------- German ----------\n\"{language}\" ist keine der unterstützten Sprachen: DE | EN\nBitte tragen Sie die gewollte Sprache, wie in dem internationalem Standard ISO 639-1 beschrieben, ein.\n\n---------- English ----------\n\"{language}\" is not one of the valid languages: DE | EN\nPlease insert the Language you want as described in the ISO 639-1 international standard.\n");
+                        Console.WriteLine($"\n\n---------- German ----------\n\"{language}\" ist keine der unterstützten Sprachen: DE | EN\nBitte tragen Sie die gewollte Sprache, wie in dem internationalem Standard ISO 639-1 beschrieben, ein.\n\n---------- English ----------\n\"{language}\" is not one of the valid languages: DE | EN\nPlease insert the Language you want as described in the ISO 639-1 international standard.");
+                        Environment.Exit(1);
+                        break;
                 }
                 this.ShlindexFileLocation = shlndexFileLocation;
                 this.ShoppingListDirectory = shoppingListDirectory;
@@ -106,7 +110,8 @@ namespace Manuel
             Content config = JsonSerializer.Deserialize<Content>(configString);
             if (string.IsNullOrWhiteSpace(config.Language))
             {
-                throw new ArgumentException($"\n\n---------- German ----------\n\"Es ist keine der unterstützten Sprachen (DE | EN) in der Konfigurationsdatei festgelegt.\nBitte tragen Sie die gewollte Sprache, wie in dem internationalem Standard ISO 639-1 beschrieben, ein.\n\n---------- English ----------\nThere is no language (DE | EN) specified inside of the config file.\nPlease insert the Language you want as described in the ISO 639-1 international standard.\n");
+                Console.WriteLine($"\n\n---------- German ----------\n\"Es ist keine der unterstützten Sprachen (DE | EN) in der Konfigurationsdatei festgelegt.\nBitte tragen Sie die gewollte Sprache, wie in dem internationalem Standard ISO 639-1 beschrieben, ein.\n\n---------- English ----------\nThere is no language (DE | EN) specified inside of the config file.\nPlease insert the Language you want as described in the ISO 639-1 international standard.");
+                Environment.Exit(1);
             }
             return config;
         }
