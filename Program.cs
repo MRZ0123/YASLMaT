7ae7a5a21fe8d49163cbd3cd3218f88d22bec38b
MRZ0123 <manuelmarz2004@gmail.com>
2023-12-06 11:45:55 +0100

    better debug messages

--- Program.cs
+++ Program.cs
@@ -19,14 +19,14 @@ namespace YASLMAT
             string configFileLocation = "./conf.ig";
 
             // ***** DEBUG MSG *****
-            if (DEBUG) { Console.WriteLine($"Checking for config file at \"{configFileLocation}\"."); }
+            if (DEBUG) { Console.WriteLine($"DEBUG:\tChecking for config file at \"{configFileLocation}\"."); }
             // **   -**#####**-   **
 
             if (!Config.Check(configFileLocation))
             {
 
                 // ***** DEBUG MSG *****
-                if (DEBUG) { Console.WriteLine($"No config present.\nCreating config at \"{configFileLocation}\"."); }
+                if (DEBUG) { Console.WriteLine($"DEBUG:\tNo config present.\nCreating config at \"{configFileLocation}\"."); }
                 // **   -**#####**-   **
 
                 Config.Create(configFileLocation);
@@ -34,32 +34,32 @@ namespace YASLMAT
             else
             {
                 // ***** DEBUG MSG *****
-                if (DEBUG) { Console.WriteLine($"A config has been found."); }
+                if (DEBUG) { Console.WriteLine($"DEBUG:\tA config has been found."); }
                 // **   -**#####**-   **
             }
-            
+
             // ***** DEBUG MSG *****
-            if (DEBUG) {Console.WriteLine($"Reading from config."); }
+            if (DEBUG) { Console.WriteLine($"DEBUG:\tReading from config."); }
             // **   -**#####**-   **
-            
+
             Config.Content currentConfig = Config.Read(configFileLocation);
 
             // ***** DEBUG MSG *****
-            if (DEBUG) { Console.WriteLine($"Current config content is:\n{currentConfig}"); }
+            if (DEBUG) { Console.WriteLine($"DEBUG:\tCurrent config content is:\n{currentConfig}"); }
             // **   -**#####**-   **
 
-            
+
             // ***** DEBUG MSG *****
-            if (DEBUG) {Console.WriteLine($"Checking if there's an Shlindex file."); }
+            if (DEBUG) { Console.WriteLine($"DEBUG:\tChecking if there's an Shlindex file."); }
             // **   -**#####**-   **
-            
+
             if (!Shlindex.Check(currentConfig))
             {
-                
+
                 // ***** DEBUG MSG *****
-                if (DEBUG) {Console.WriteLine($"No Shlindexfile found at\"{currentConfig.ShlindexFileLocation}\""); }
+                if (DEBUG) { Console.WriteLine($"DEBUG:\tNo Shlindexfile found at\"{currentConfig.ShlindexFileLocation}\""); }
                 // **   -**#####**-   **
-                
+
                 Console.WriteLine(currentConfig.Language == "DE" ? "Es scheint, als wäre es das erste Mal, dass Sie dieses Programm gestartet haben.\nViel Spaß beim Benutzen dieses Programmes!" : "This seems to be the first time you have started this application.\nHave fun using this application!");
             }
         }
