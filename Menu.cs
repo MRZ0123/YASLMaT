189c5e7dce05dbfffef8f5b5ed5a0bcd5c6edf38
dasjugilein <jugihensel@gmail.com>
2023-12-07 23:16:10 +0100

    added Menu.cs file

---
+++ Menu.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Team
+{
+  class Menu
+  {
+    /** Added by: Jugi
+     * 
+     * function used to display the commandline menu
+     * 
+     */
+    public static void Display()
+    {
+      // your code here
+    }
+  }
+}
