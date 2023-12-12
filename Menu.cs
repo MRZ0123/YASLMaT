cfd0d55e96820cbf82fb8a3e42c847d484003f55
Mena`♡ <40590467+MRZ0123@users.noreply.github.com>
2023-12-12 15:22:17 +0100

    formatted by removing newlines Menu.cs

--- Menu.cs
+++ Menu.cs
@@ -10,19 +10,12 @@ namespace Team
      * function used to display the commandline menu
      * 
      */
-     
-
     public static void Display(Config.Content currentConfig)
     {
       Console.WriteLine(currentConfig.Language == "DE" ? "Hauptmenü" : "Main menu");
       Console.WriteLine(currentConfig.Language == "DE" ? "1. Liste erstellen" : "1. Create list");
       Console.WriteLine(currentConfig.Language == "DE" ? "2. Alle vorhandenen Listen anzeigen" : "2. Show existing lists");
       Console.WriteLine(currentConfig.Language == "DE" ? "3. Beenden" : "3. Quit");
-
     }
-
-    
-
-  
   }
 }
