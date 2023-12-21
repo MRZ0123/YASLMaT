f33a2b5dec71015004104d864d1279dfa2eec27a
MRZ0123 <manuelmarz2004@gmail.com>
2023-12-21 14:31:34 +0100

    fixed typo

--- Menu.cs
+++ Menu.cs
@@ -303,11 +303,12 @@ namespace Team
     public static void DisplayShoppingListContent(Config.Content currentConfig, ShoppingList.Content shlistContent)
     {
       Console.WriteLine($"{(currentConfig.Language == "DE" ? "Liste: " : "list: ")}{shlistContent.Metadata.Name,30}{"; " + (shlistContent.Metadata.Shop == "" ? (currentConfig.Language == "DE" ? "Kein Laden spezifiziert." : "no shop specified") : shlistContent.Metadata.Shop)}{"; " + (currentConfig.Language == "DE" ? "Artikel: " : "Items: ")}{shlistContent.Metadata.FullItemCount}");
-      if ( shlistContent.Items.Count > 0) { Console.WriteLine("\n"); }
+      if (shlistContent.Items.Count > 0) { Console.WriteLine("\n"); }
       for (int i = 0; i < shlistContent.Items.Count; i++)
       {
+        // use [i] instead of .elementAt(i) for performance reasons
         ShoppingList.Item currentItem = shlistContent.Items[i];
-        Console.WriteLine($"({i + "):",5}{(currentConfig.Language == "DE" ? "Artikel: " : "Item: ")}{currentItem.ItemName,-30}{(currentConfig.Language == "DE" ? "Menge: " : "amount: ")}{currentItem.ItemCount, -5}");
+        Console.WriteLine($"({i + "):",5}{(currentConfig.Language == "DE" ? "Artikel: " : "Item: ")}{currentItem.ItemName,-30}{(currentConfig.Language == "DE" ? "Menge: " : "amount: ")}{currentItem.ItemCount,-5}");
       }
     }
 
@@ -327,7 +328,6 @@ namespace Team
       {
         for (int i = 0; i < index.MetadataShlindex.Count; i++)
         {
-          // use [i] instead of .elementAt(i) for performance reasons
           ShoppingList.Metadata currentMetadata = index.MetadataShlindex[i];
           Console.WriteLine($"{$"({i + "):",5} {(currentConfig.Language == "DE" ? "Name: " : "name: ")}{currentMetadata.Name,30}{"; " + (currentMetadata.Shop == "" ? (currentConfig.Language == "DE" ? "Kein Laden spezifiziert." : "no shop specified") : currentMetadata.Shop)}",-80}{(currentConfig.Language == "DE" ? "Artikel: " : "Items: ")}{currentMetadata.FullItemCount}");
         }
@@ -342,7 +342,7 @@ namespace Team
       */
     private static void DisplayItemNameQuestion(Config.Content currentConfig)
     {
-      Console.WriteLine(currentConfig.Language == "DE" ? "Bitte geben Sie einen Artiekl ein:" : "Please enter a shopping item:");
+      Console.WriteLine(currentConfig.Language == "DE" ? "Bitte geben Sie einen Artikel ein:" : "Please enter a shopping item:");
     }
 
 
